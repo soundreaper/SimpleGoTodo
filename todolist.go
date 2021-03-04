7c6a891d9a500322cfb758a56e2d72b1edb9954d
soundreaper@gmail.com <soundreaper@gmail.com>
2021-03-05 02:47:40 +0545

    add create function

--- todolist.go
+++ todolist.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"net/http"
 
+	"encoding/json"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -13,12 +15,25 @@ import (
 
 var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
 
+// TodoItemModel is the structure for how a todo list item should look
 type TodoItemModel struct {
-	Id          int `gorm:"primary_key"`
+	ID          int `gorm:"primary_key"`
 	Description string
 	Completed   bool
 }
 
+// CreateItem is a function that creates an item in the database.
+func CreateItem(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem. Saving to database.")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
+}
+
+// APIStatus is a function that checks to see if the backend of the application is running
 func APIStatus(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
 	w.Header().Set("Content-Type", "application/json")
@@ -38,5 +53,6 @@ func main() {
 	log.Info("Starting Todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/apistatus", APIStatus).Methods("GET")
+	router.HandleFunc("/todo", CreateItem).Methods("POST")
 	http.ListenAndServe(":8000", router)
 }
