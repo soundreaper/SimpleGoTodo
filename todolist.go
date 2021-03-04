2e7f42f157eebf2c00ddac967c16140594d24558
soundreaper@gmail.com <soundreaper@gmail.com>
2021-03-05 03:01:54 +0545

    add remaining CRUD functionality and helper functions for grabbing todo items

--- todolist.go
+++ todolist.go
@@ -6,6 +6,8 @@ import (
 
 	"encoding/json"
 
+	"strconv"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -17,20 +19,96 @@ var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&
 
 // TodoItemModel is the structure for how a todo list item should look
 type TodoItemModel struct {
-	ID          int `gorm:"primary_key"`
+	Id          int `gorm:"primary_key"`
 	Description string
 	Completed   bool
 }
 
-// CreateItem is a function that creates an item in the database.
+// CreateItem is a function that creates a todo item in the database.
 func CreateItem(w http.ResponseWriter, r *http.Request) {
 	description := r.FormValue("description")
-	log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem. Saving to database.")
+	log.WithFields(log.Fields{"description": description}).Info("Add new Todo item. Saving to database.")
 	todo := &TodoItemModel{Description: description, Completed: false}
 	db.Create(&todo)
 	result := db.Last(&todo)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(result.Value)
+}
+
+// UpdateItem is a function that updates a todo item in the database.
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+	// Get URL parameter from mux
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	// Check if the todo item exists in DB
+	err := GetItemByID(id)
+	if err == false {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": false, "error": "Record Not Found"}`)
+	} else {
+		completed, _ := strconv.ParseBool(r.FormValue("completed"))
+		log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating todo item")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		todo.Completed = completed
+		db.Save(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": true}`)
+	}
+}
+
+// DeleteItem is a function that deletes a todo item in the database.
+func DeleteItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	err := GetItemByID(id)
+	if err == false {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"deleted": false, "error": "Record Not Found"}`)
+	} else {
+		log.WithFields(log.Fields{"Id": id}).Info("Deleting todo item")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		db.Delete(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"deleted": true}`)
+	}
+}
+
+// GetItemByID is a function that grabs a todo item from the database via ID.
+func GetItemByID(Id int) bool {
+	todo := &TodoItemModel{}
+	result := db.First(&todo, Id)
+	if result.Error != nil {
+		log.Warn("todo item not found in database")
+		return false
+	}
+	return true
+}
+
+// GetCompletedItems is a function that grabs todo items from the database that are marked completed.
+func GetCompletedItems(w http.ResponseWriter, r *http.Request) {
+	log.Info("Get completed todo items")
+	completedTodoItems := GetTodoItems(true)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(completedTodoItems)
+}
+
+// GetIncompleteItems is a function that grabs todo items from the database that are marked incompleted.
+func GetIncompleteItems(w http.ResponseWriter, r *http.Request) {
+	log.Info("Get incomplete todo items")
+	IncompleteTodoItems := GetTodoItems(false)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(IncompleteTodoItems)
+}
+
+// GetTodoItems is a function that grabs todo items from the database whose completion status is unknown.
+func GetTodoItems(completed bool) interface{} {
+	var todos []TodoItemModel
+	TodoItems := db.Where("completed = ?", completed).Find(&todos).Value
+	return TodoItems
 }
 
 // APIStatus is a function that checks to see if the backend of the application is running
@@ -50,9 +128,13 @@ func main() {
 	db.Debug().DropTableIfExists(&TodoItemModel{})
 	db.Debug().AutoMigrate(&TodoItemModel{})
 
-	log.Info("Starting Todolist API server")
+	log.Info("Starting todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/apistatus", APIStatus).Methods("GET")
+	router.HandleFunc("/todo-completed", GetCompletedItems).Methods("GET")
+	router.HandleFunc("/todo-incomplete", GetIncompleteItems).Methods("GET")
 	router.HandleFunc("/todo", CreateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", UpdateItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", DeleteItem).Methods("DELETE")
 	http.ListenAndServe(":8000", router)
 }
